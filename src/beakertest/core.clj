09ad916b66f31ab403faba429ac1d3fa7abd9405
Dan McShan <dan@xybotyx.com>
2017-01-16 14:01:43 -0700

    Fixed test source

--- src/beakertest/core.clj
+++ src/beakertest/core.clj
@@ -1,12 +1,14 @@
 (ns beakertest.core
- (:require [clj-time.coerce :refer [from-date]])
-)
+ (:require [clj-time.coerce :refer [from-date from-long to-date]]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
-  (= org.joda.time.DateTime (type x)) (encode [(str x) "xsd:dateTime"])
-  (println x "Hello, World!"))
+  
+  (println x "Hello, World!")
+  (let [dt (from-long 893462400000)
+        d  (to-date dt)]
+    (= dt (from-date d))))
 
 
 
